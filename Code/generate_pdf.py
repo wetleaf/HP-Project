8493c8949e23aeb4ff9e748cca17dc9e9eef50de
wetleaf <harsh1513088@gmail.com>
2023-11-02 16:23:32 +0530

    Removed Temp Dir

---
+++ Code/generate_pdf.py
@@ -0,0 +1,29 @@
+from pylatex import Document, Section, Subsection, Command
+from pylatex.utils import italic, NoEscape
+
+
+def fill_document(doc):
+    """Add a section, a subsection and some text to the document.
+
+    :param doc: the document
+    :type doc: :class:`pylatex.document.Document` instance
+    """
+    with doc.create(Section('A section')):
+        doc.append('Some regular text and some ')
+        doc.append(italic('italic text. '))
+
+        with doc.create(Subsection('A subsection')):
+            doc.append('Also some crazy characters: $&#{}')
+
+
+
+if __name__ == '__main__':
+    # Basic document
+    doc = Document('test')
+
+    # doc.preamble.append(border)
+    doc.preamble.append(Command('usepackage',r'pgf'))
+    doc.preamble.append(Command('usepackage',r'pgfpages'))
+    doc.preamble.append(Command('pgfpagesdeclarelayout',r'boxed',extra_arguments=[Command('edef'),Command('pgfpageoptionborder','0pt')]))
+    doc.generate_pdf()
+    doc.generate_tex()
